c70d31c1cbf76f8f9b5ad45ee9a86bc3e7ee3806
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2019-01-24 21:32:51 -0500

    adding signup

--- wags_ground/routes/api/sign_up.js
+++ wags_ground/routes/api/sign_up.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/sign_up',(req,res,next)=>{
+router.get('/',(req,res,next)=>{
   res.status(200).json({
     message: "this route works"
   });
 });
+
+router.post('/',(req,res,next)=>{
+  res.status(200).json({
+    message: "you tried to post"
+  });
+});
+
+module.export = router;
